2f0409ebb4a64b343d238fc8ec9b2bfca9066de5
itaitoshiki <toshiki.i0918@gmail.com>
2021-11-15 03:27:51 +0900

    ✨ ogp:image 追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const MyDocument = () => {
   const title = 'チョーセイ';
   const description = 'LINE上で簡単に時間単位の日程調整ができるアプリです。';
+  const url = 'https://chousei.vercel.app/';
 
   return (
     <Html lang="ja-JP">
@@ -11,7 +12,7 @@ const MyDocument = () => {
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:site_name" content={title} />
-        {/* <meta property="og:image" content={`${url}/ogp.png`} /> */}
+        <meta property="og:image" content={`${url}/ogp.png`} />
         <meta name="format-detection" content="telephone=no" />
       </Head>
       <body>
