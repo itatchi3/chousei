765716b97ad66bac2f0334d8a3c031d6edcc39e1
itaitoshiki <toshiki.i0918@gmail.com>
2021-11-15 03:45:22 +0900

    🐛 ogp画像

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const MyDocument = () => {
   const title = 'チョーセイ';
   const description = 'LINE上で簡単に時間単位の日程調整ができるアプリです。';
-  const url = 'https://chousei.vercel.app/';
+  const url = 'https://chousei.vercel.app';
 
   return (
     <Html lang="ja-JP">
@@ -12,7 +12,7 @@ const MyDocument = () => {
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:site_name" content={title} />
-        <meta property="og:image" content={`${url}/ogp.png`} />
+        <meta property="og:image" content={`${url}/ogp.PNG`} />
         <meta name="format-detection" content="telephone=no" />
       </Head>
       <body>
