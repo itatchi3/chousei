abec0dbb6b0c7a2e7dd5beadf98f9ba53e28f7cc
itaitoshiki <toshiki.i0918@gmail.com>
2021-11-18 19:08:12 +0900

    🎨 ogpのurl変更

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const MyDocument = () => {
   const title = 'チョーセイ';
   const description = 'LINE上で簡単に時間単位の日程調整ができるアプリです。';
-  const url = 'https://chousei.vercel.app';
+  const url = 'https://liff.line.me/1656098585-v7VEeZ7Q';
 
   return (
     <Html lang="ja-JP">
