b39105118f6294a273a1436bc560cd187b217d8f
itaitoshiki <toshiki.i0918@gmail.com>
2021-11-29 02:05:05 +0900

    🐛 NEXT_PUBLIC_VERCEL_URL

--- lib/getVercelUrl.ts
+++ lib/getVercelUrl.ts
@@ -1,13 +1,14 @@
 export const getVercelUrl = () => {
-  if (!process.env.VERCEL_URL) {
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
+  if (!process.env.NEXT_PUBLIC_VERCEL_URL) {
     return 'http://localhost:3000';
   }
 
-  if (/localhost/.test(process.env.VERCEL_URL)) {
+  if (/localhost/.test(process.env.NEXT_PUBLIC_VERCEL_URL)) {
     return 'http://localhost:3000';
   }
 
-  return /^http/.test(process.env.VERCEL_URL)
+  return /^http/.test(process.env.NEXT_PUBLIC_VERCEL_URL)
     ? process.env.VERCEL_URL
-    : `https://${process.env.VERCEL_URL}`;
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_URLL}`;
 };
