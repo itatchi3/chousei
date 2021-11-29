05c8707ff51ef73ae7877d616c176be21ac1a199
itaitoshiki <toshiki.i0918@gmail.com>
2021-11-29 23:43:28 +0900

    ✨ イベントを確認したかチェック

--- src/pages/event/[id]/index.tsx
+++ src/pages/event/[id]/index.tsx
@@ -3,6 +3,8 @@ import { GetServerSideProps } from 'next';
 import { EventDetail } from 'src/components/page/EventDetail';
 import { prisma } from 'lib/prisma';
 import superjson from 'superjson';
+import { useLiff } from 'src/liff/auth';
+import { useEffect } from 'react';
 
 type Props = {
   eventData: string;
@@ -10,6 +12,22 @@ type Props = {
 
 const Event = (props: Props) => {
   const eventData: EventType = superjson.parse(props.eventData);
+  const { idToken } = useLiff();
+
+  useEffect(() => {
+    const createParticipate = async () => {
+      if (!eventData || !idToken) return;
+      try {
+        await fetch('/api/createParticipate', {
+          method: 'POST',
+          body: JSON.stringify({ idToken, eventId: eventData.id }),
+        });
+      } catch (error) {
+        alert(error);
+      }
+    };
+    createParticipate();
+  }, [eventData, idToken]);
 
   return <EventDetail eventData={eventData} />;
 };
