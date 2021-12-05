2a25d6fb89fb5cdbc6b258699311f4b380783887
itaitoshiki <toshiki.i0918@gmail.com>
2021-12-06 00:44:57 +0900

    ⚡️ 少し入力画面のパフォーマンスを上げた

--- src/components/InputSchedule/InputSchedule.tsx
+++ src/components/InputSchedule/InputSchedule.tsx
@@ -23,6 +23,8 @@ export const InputSchedule = () => {
   const [loading, setLoading] = useState(false);
   const [dates, setDates] = useState<Date[]>([]);
   const [dateStrings, setDateStrings] = useState<string[]>([]);
+  const [hiddenDates, setHiddenDates] = useState<string[]>([]);
+  const [dateWidth, setDateWidth] = useState(0);
   const [eventFullCalendar, setEventFullCalendar] = useState<EventFullCalendar[]>([]);
   const [minTime, setMinTime] = useState(0);
   const [maxTime, setMaxTime] = useState(24);
@@ -34,8 +36,6 @@ export const InputSchedule = () => {
   const [firstVoteList, setFirstVoteList] = useState<{ id: number; vote: string }[]>();
 
   const router = useRouter();
-
-  const hiddenDate = [];
 
   const checkColor = (vote: string) => {
     switch (vote) {
@@ -131,10 +131,47 @@ export const InputSchedule = () => {
     }
   };
 
+  const fullCalendarStyle = () => {
+    const numberOfDates = dates.length;
+    let widthStyle = '';
+    if (numberOfDates >= 4) {
+      widthStyle = `.fc-scrollgrid, .fc-scrollgrid table {width: ${
+        100 * numberOfDates
+      }px !important;}`;
+    }
+
+    let hiddenStyle = '';
+    hiddenDates.map((hiddenDate) => {
+      hiddenStyle += `[data-date='${hiddenDate}'] {display: none !important}`;
+    });
+    return (
+      <style jsx global>
+        {`
+          ${widthStyle}
+          ${hiddenStyle}
+          .fc-scrollgrid thead {
+            display: none !important;
+          }
+          .fc-timegrid-event .fc-event-time {
+            white-space: normal;
+            font-size: 10px;
+          }
+          .fc-day-today {
+            background-color: white !important;
+          }
+          .fc-timegrid-slot-label {
+            display: none !important;
+          }
+        `}
+      </style>
+    );
+  };
+
   useEffect(() => {
     if (!event || !firstVoteList) return;
     let dateList: Date[] = [event.possibleDates[0].date];
     let dateStringList: string[] = [event.possibleDates[0].dateString];
+    let dateTimeList: number[] = [event.possibleDates[0].date.getTime()];
     let eventFullCalendar: EventFullCalendar[] = [];
     let newMinTime = 24;
     let newMaxTime = 0;
@@ -156,34 +193,15 @@ export const InputSchedule = () => {
         id: index.toString(),
         color: checkColor(firstVoteList[index].vote),
       });
-      // if (dateStringList.includes(possibleDate.dateString)) {
-      //   eventFullCalendar.push({
-      //     start: possibleDate.startTime,
-      //     end: possibleDate.endTime,
-      //     id: index.toString(),
-      //     color: checkColor(firstVoteList[index].vote),
-      //   });
-      // } else {
-      //   eventFullCalendarList.push(eventFullCalendar);
-      //   dateList.push(possibleDate.date);
-      //   dateStringList.push(possibleDate.dateString);
-      //   eventFullCalendar = [
-      //     {
-      //       start: possibleDate.startTime,
-      //       end: possibleDate.endTime,
-      //       id: index.toString(),
-      //       color: checkColor(firstVoteList[index].vote),
-      //     },
-      //   ];
-      // }
+      if (!dateStringList.includes(possibleDate.dateString)) {
+        dateList.push(possibleDate.date);
+        dateStringList.push(possibleDate.dateString);
+      }
+      dateTimeList.push(possibleDate.date.getTime());
     });
-    // if (eventFullCalendar.length) {
-    //   eventFullCalendarList.push(eventFullCalendar);
-    // }
     setDates(dateList);
     setDateStrings(dateStringList);
     setEventFullCalendar(eventFullCalendar);
-    // setEventFullCalendarList(eventFullCalendarList);
     setMinTime(newMinTime);
     if (minutesWhenMaxTime) {
       newMaxTime += 1;
@@ -194,6 +212,19 @@ export const InputSchedule = () => {
       newTimeList.push(i);
     }
     setViewTimeList(newTimeList);
+
+    const dateWidth = (dateTimeList[dateTimeList.length - 1] - dateTimeList[0]) / 86400000;
+    let hiddenDates: string[] = [];
+    for (let i = 1; i < dateWidth; i++) {
+      let date = cloneDeep(dateList[0]);
+      date.setDate(date.getDate() + i);
+      if (!dateTimeList.includes(date.getTime())) {
+        const hiddenDate = date.toISOString().slice(0, 10);
+        hiddenDates.push(hiddenDate);
+      }
+    }
+    setDateWidth(dateWidth + 1);
+    setHiddenDates(hiddenDates);
   }, [event, firstVoteList]);
 
   useEffect(() => {
@@ -211,109 +242,64 @@ export const InputSchedule = () => {
     setVoteList(voteList);
     setFirstVoteList(voteList);
   }, [event, userId]);
-
   return (
     <Box p="3">
-      <Flex height={window.innerHeight - 150} overflow="scroll">
-        <VStack pt="4" pr="2">
-          {viewTimeList.map((viewTime) => (
-            <Box key={viewTime} fontSize="xs" pb="6">
-              {viewTime + ':00'}
-            </Box>
-          ))}
-        </VStack>
-        <FullCalendar
-          plugins={[timeGridPlugin]}
-          initialView="timeGridWeek"
-          headerToolbar={false}
-          allDaySlot={false}
-          contentHeight={'auto'}
-          // initialDate={new Date(eventFullCalendarList[0][0].)}
-          events={eventFullCalendar}
-          slotEventOverlap={false}
-          eventTimeFormat={{
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: false,
-          }}
-          eventClick={(arg) => onClickVoteChange(arg)}
-          slotMinTime={minTime + ':00:00'}
-          slotMaxTime={maxTime + ':00:00'}
-        />
+      <Box height={window.innerHeight - 150} overflow="scroll">
+        <Flex>
+          <Flex flexDirection="column" pt="3" pr="2">
+            {viewTimeList.map((viewTime) => (
+              <Box key={viewTime} fontSize="xs" pb="8">
+                {viewTime + ':00'}
+              </Box>
+            ))}
+          </Flex>
+          <Box>
+            {dateStrings.length > 0 && (
+              <Flex>
+                {dateStrings.map((dateString, index) => (
+                  <Center w="100%" fontWeight="bold" fontSize="sm" key={index}>
+                    {dateString}
+                  </Center>
+                ))}
+              </Flex>
+            )}
 
-        {/* {dates.map((date, index) => {
-          return (
-            <Box minWidth="100px" key={index}>
-              <Center fontWeight="bold">{dateStrings[index]}</Center>
+            {dateWidth > 0 && dates.length > 0 && (
               <FullCalendar
                 plugins={[timeGridPlugin]}
-                initialView="timeGridWeek"
+                initialView="timeGrid"
+                duration={{ days: dateWidth }}
                 headerToolbar={false}
                 allDaySlot={false}
                 contentHeight={'auto'}
-                initialDate={new Date(date)}
-                events={eventFullCalendarList[index]}
+                initialDate={dates[0]}
+                events={eventFullCalendar}
                 slotEventOverlap={false}
                 eventTimeFormat={{
                   hour: '2-digit',
                   minute: '2-digit',
                   hour12: false,
                 }}
-                eventClick={(arg) => onClickVoteChange(arg, index)}
+                eventClick={(arg) => onClickVoteChange(arg)}
                 slotMinTime={minTime + ':00:00'}
                 slotMaxTime={maxTime + ':00:00'}
               />
-            </Box>
-          );
-        })} */}
-        {dates.length >= 4 && (
-          <VStack pt="4" pl="2">
-            {viewTimeList.map((viewTime) => (
-              <Box key={viewTime} fontSize="xs" pb="6">
-                {viewTime + ':00'}
-              </Box>
-            ))}
-          </VStack>
-        )}
-        {/* 
-        <style jsx>{`
-          .fc-timegrid-slot-label,
-          thead {
-            display: none !important;
-          }
-          .fc-timegrid-event .fc-event-time {
-            white-space: normal;
-            font-size: 10px;
-          }
-          .fc-timegrid-slot {
-            background-color: white;
-          }
-        `}</style> */}
+            )}
+          </Box>
 
-        {/* {hiddenDate.map((date, index) => {
-          return (
-            <style jsx global key={index}>
-              {`
-                [data-date='${date}'] {
-                  display: none !important;
-                }
-                ,
-                .fc-timegrid-slot-label,
-                thead {
-                  display: none !important;
-                }
-                .fc-timegrid-event .fc-event-time {
-                  white-space: normal;
-                  font-size: 10px;
-                }
-                .fc-timegrid-slot {
-                  background-color: white;
-                }
-              `}
-            </style>
-          );
-        })} */}
-      </Flex>
+          {dates.length >= 4 && (
+            <Flex flexDirection="column" pt="3" pl="2">
+              {viewTimeList.map((viewTime) => (
+                <Box key={viewTime} fontSize="xs" pb="8">
+                  {viewTime + ':00'}
+                </Box>
+              ))}
+            </Flex>
+          )}
+          {fullCalendarStyle()}
+        </Flex>
+      </Box>
+
       <Center>
         <VStack pos="fixed" bottom="0" bg="white" w="100%">
           <HStack p="4" spacing={4}>
