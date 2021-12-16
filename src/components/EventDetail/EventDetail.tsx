ea9c10893e6ad5117f29dfd52538631ccbfd151e
itaitoshiki <toshiki.i0918@gmail.com>
2021-12-16 19:04:24 +0900

    ✨ 友達登録していない人にポップアップ表示

--- src/components/EventDetail/EventDetail.tsx
+++ src/components/EventDetail/EventDetail.tsx
@@ -1,8 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { AttendanceTable } from 'src/components/EventDetail/AttendanceTable';
 import { useSetRecoilState } from 'recoil';
 import { eventState } from 'src/atoms/eventState';
-import { Box, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Link,
+  ModalFooter,
+  VStack,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react';
 import { ShareButton } from 'src/components/EventDetail/ShareButton';
 import { MoveAnswerScheduleButton } from 'src/components/EventDetail/MoveAnswerScheduleButton';
 import { AnswerComment } from 'src/components/EventDetail/AnswerComment';
@@ -17,7 +30,8 @@ type Props = {
 
 export const EventDetail = ({ eventDetailData }: Props) => {
   const setEvent = useSetRecoilState(eventState);
-  const { idToken, userId } = useLiff();
+  const { liff, idToken, userId } = useLiff();
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     setEvent(eventDetailData.eventData);
@@ -73,6 +87,19 @@ export const EventDetail = ({ eventDetailData }: Props) => {
     }
   }, [eventDetailData, idToken, userId]);
 
+  useEffect(() => {
+    if (!liff) return;
+    const func = async () => {
+      try {
+        const friendFlag = await liff.getFriendship();
+        setIsOpen(!friendFlag.friendFlag);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    func();
+  }, [liff]);
+
   return (
     <Box p="3">
       <EventOverview
@@ -94,6 +121,37 @@ export const EventDetail = ({ eventDetailData }: Props) => {
           <ShareButton />
         </Box>
       </VStack>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>おすすめ</ModalHeader>
+          <ModalBody>
+            <Box fontWeight="bold">
+              チョーセイ公式アカウントと友だちになると、イベントが作成できるようになります。
+            </Box>
+            <Flex direction="column" alignItems="center" pt="7">
+              <Image borderRadius="full" src="/chouseiIcon.PNG" alt="アイコン" width="80px" />
+              <Box fontWeight="bold" pt="2" pb="5">
+                チョーセイ
+              </Box>
+
+              <Link href="https://lin.ee/InOsTpg" sx={{ _focus: { outline: 'none !important' } }}>
+                <Image
+                  src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png"
+                  alt="友だち追加"
+                  width="140px"
+                />
+              </Link>
+            </Flex>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variant="ghost" onClick={() => setIsOpen(false)}>
+              キャンセル
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
