2e828ff009a8cac431ea8864e0193dd34bbc09e2
itaitoshiki <toshiki.i0918@gmail.com>
2021-12-23 21:48:52 +0900

    ⚡️ バリデーション追加

--- src/components/CreateEvent/InputEventDiscription.tsx
+++ src/components/CreateEvent/InputEventDiscription.tsx
@@ -1,6 +1,13 @@
 import { useRecoilState } from 'recoil';
 import { overViewState } from 'src/atoms/eventState';
-import { Box, FormControl, FormHelperText, FormLabel, Textarea } from '@chakra-ui/react';
+import {
+  Box,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Textarea,
+} from '@chakra-ui/react';
 
 export const InputEventDiscription = () => {
   const [event, setEvent] = useRecoilState(overViewState);
@@ -12,14 +19,30 @@ export const InputEventDiscription = () => {
     }));
   };
   return (
-    <FormControl px="3" py="2" borderWidth="2px" borderRadius="lg">
+    <FormControl
+      px="3"
+      py="2"
+      borderWidth="2px"
+      borderRadius="lg"
+      isInvalid={event.description.length > 255}
+    >
       <Box py="1">
         <FormLabel fontWeight="bold" fontSize="sm">
           補足・備考
         </FormLabel>
       </Box>
-      <Textarea onChange={changeDescription} value={event.description} rows={4} />
-      <FormHelperText fontSize="xs">〆切は○日など</FormHelperText>
+      <Textarea
+        isInvalid={event.description.length > 255}
+        onChange={changeDescription}
+        value={event.description}
+        rows={4}
+      />
+
+      {event.description.length <= 255 ? (
+        <FormHelperText fontSize="xs">〆切は○日など</FormHelperText>
+      ) : (
+        <FormErrorMessage fontSize="xs">255文字以内にしてください</FormErrorMessage>
+      )}
     </FormControl>
   );
 };
