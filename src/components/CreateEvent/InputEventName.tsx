aa98e84e09dba2147edaff7666bb9d5d1bdf2584
itaitoshiki <toshiki.i0918@gmail.com>
2021-12-23 21:48:52 +0900

    ⚡️ バリデーション追加

--- src/components/CreateEvent/InputEventName.tsx
+++ src/components/CreateEvent/InputEventName.tsx
@@ -1,6 +1,13 @@
 import { useRecoilState } from 'recoil';
 import { overViewState } from 'src/atoms/eventState';
-import { Box, FormControl, FormHelperText, FormLabel, Input } from '@chakra-ui/react';
+import {
+  Box,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react';
 
 export const InputEventName = () => {
   const [event, setEvent] = useRecoilState(overViewState);
@@ -12,14 +19,30 @@ export const InputEventName = () => {
     }));
   };
   return (
-    <FormControl px="3" py="2" borderWidth="2px" borderRadius="lg" isRequired>
+    <FormControl
+      px="3"
+      py="2"
+      borderWidth="2px"
+      borderRadius="lg"
+      isRequired
+      isInvalid={event.eventName.length > 255}
+    >
       <Box py="1">
         <FormLabel fontWeight="bold" fontSize="sm">
           イベント名
         </FormLabel>
       </Box>
-      <Input onChange={changeEventname} value={event.eventName} />
-      <FormHelperText fontSize="xs">ミーティング、練習日程、シフト調整など</FormHelperText>
+      <Input
+        isInvalid={event.eventName.length > 255}
+        onChange={changeEventname}
+        value={event.eventName}
+      />
+
+      {event.eventName.length <= 255 ? (
+        <FormHelperText fontSize="xs">ミーティング、練習日程、シフト調整など</FormHelperText>
+      ) : (
+        <FormErrorMessage fontSize="xs">255文字以内にしてください</FormErrorMessage>
+      )}
     </FormControl>
   );
 };
