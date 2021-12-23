2803a5d50020f290a540804eac43b3e2bcbab27e
itaitoshiki <toshiki.i0918@gmail.com>
2021-12-23 19:14:19 +0900

    ✨ データが無いときの情報を表示

--- src/pages/event/[id]/edit.tsx
+++ src/pages/event/[id]/edit.tsx
@@ -1,7 +1,25 @@
+import { Box, Flex } from '@chakra-ui/react';
+import { useRecoilValue } from 'recoil';
+import { eventState } from 'src/atoms/eventState';
 import { EditEvent } from 'src/components/EditEvent/EditEvent';
 
 const Edit = () => {
-  return <EditEvent />;
+  const event = useRecoilValue(eventState);
+  return (
+    <>
+      {event ? (
+        <EditEvent />
+      ) : (
+        <Flex height="100vh" px="5" justifyContent="center" alignItems="center">
+          <Box fontWeight="bold">
+            イベントデータが読み込めませんでした。
+            <br />
+            イベント詳細画面へ戻ってください。
+          </Box>
+        </Flex>
+      )}
+    </>
+  );
 };
 
 export default Edit;
